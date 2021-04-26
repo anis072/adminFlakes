3b24e5bba65c1d2df409d7355a276fa7a65596f9
anis khalfaoui <aniskhalfaouiisi@gmail.com>
2021-04-26 21:54:43 +0000

    fix type for expertise

--- src/app/expertise/expertise-managment/expertise-managment.component.ts
+++ src/app/expertise/expertise-managment/expertise-managment.component.ts
@@ -12,8 +12,9 @@ import { ToAstrService } from 'src/app/layouts/service/toAstr.service';
 export class ExpertiseManagmentComponent implements OnInit {
   maqForm : FormGroup;
   maqEditForm : FormGroup;
-  expertise:any;
+  expertise:any={};
   itemId;
+  submited : boolean;
   event: EventEmitter<any> = new EventEmitter();
   constructor(private fb : FormBuilder,
               private modalService : BsModalService,
@@ -29,18 +30,22 @@ export class ExpertiseManagmentComponent implements OnInit {
   init_Form() {
    this.maqForm = this.fb.group({
     lebelle : ['',Validators.required],
+    type :['',Validators.required]
    });
   }
   inti_edit(){
     this.maqEditForm = this.fb.group({
       idExpertise :[JSON.stringify(this.itemId)],
-      libelle : ['',Validators.required],
+      lebelle : ['',Validators.required],
+      type :['',Validators.required]
      });
   }
   getExpertise(){
+
     this.expertiseService.getByID(this.itemId).subscribe({
       next: data=>{
         this.expertise = data;
+        console.log(this.expertise);
       },
       error :err=>{
         this.toastr.danger('Erreur','Erreur')
@@ -49,7 +54,12 @@ export class ExpertiseManagmentComponent implements OnInit {
   }
 
   submit(){
+    this.submited =true;
     if(this.itemId != ''){
+      if(this.maqEditForm.invalid){
+        return;
+      }
+      alert(JSON.stringify(this.maqEditForm.value))
       this.expertiseService.edit(this.maqEditForm.value).subscribe({
        next: data=>{
         this.toastr.success('Expertise mis a jour avec succeés','Sucess');
@@ -64,6 +74,9 @@ export class ExpertiseManagmentComponent implements OnInit {
       })
     }
     else {
+      if(this.maqForm.invalid){
+        return;
+      }
       this.expertiseService.addExpertise(this.maqForm.value).subscribe({
         next: data=>{
           this.toastr.success('Expertise ajouté avec succeés','Sucess');
